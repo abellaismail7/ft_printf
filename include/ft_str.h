f560f0b1554a1e5b2da6ce312948458463aed048
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2021-10-23 09:49:22 +0000

    add missing ft_str.c

--- include/ft_str.h
+++ include/ft_str.h
@@ -1,8 +1,7 @@
 #ifndef FT_STR_H
 # define FT_STR_H
 
-int ft_atoi(char **str);
-int ft_strncmp(char *s1, char *s2, int n);
+int ft_strncmp(char *s1, char *s2, unsigned int n);
 int ft_strlen(char *str);
 
 #endif
